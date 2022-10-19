df18224d97f47cf68068604616ab99c5ad689bf0
Zayan <particlegriffin@gmail.com>
2022-10-19 19:40:09 +0800

    made circumference calculator

---
+++ circumference.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+// using namespace std;
+
+int main()
+{
+    
+    const double PI = 3.14159;
+    double radius = 10;
+    double circumference = 2*PI*radius;
+
+    std::cout << circumference << "cm";
+    return 0;
+}
