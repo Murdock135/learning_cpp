0c46257a62b43bd7e36f25a4f528885b6e9a2cd7
Zayan <particlegriffin@gmail.com>
2022-12-08 09:55:51 +0800

    1008 assignment almost done

--- array.cpp
+++ array.cpp
@@ -1,26 +1,36 @@
 #include <iostream>
 
-// using namespace std;
+using namespace std;
 
 int main()
 {
-    std::string car[] = {"bmw", "mustang"};
-    std::cout << *car << std::endl; //one way of accessing element
-    std::cout << car[1] << std::endl; //main way of accessing element
+    //std::string car[] = {"bmw", "mustang"};
+    //std::cout << *car << std::endl; //one way of accessing element
+    //std::cout << car[1] << std::endl; //main way of accessing element
 
-    std::string cars[3]; //arrays can be declared
+    //std::string cars[3]; //arrays can be declared
 
-    cars[0] = "camaro";
-    cars[1] = "camri";
-    cars[2] = "camri";
+    //cars[0] = "camaro";
+    //cars[1] = "camri";
+    //cars[2] = "camri";
 
 
-    // iterating over an array
-    double prices[] = {5.00, 7.50, 9.99};
-    
-    for(int i=0; i< sizeof(prices)/sizeof(double); i++){
-        std::cout << prices[i] << "\n";
+    //// iterating over an array
+    //double prices[] = {5.00, 7.50, 9.99};
+    //
+    //for(int i=0; i< sizeof(prices)/sizeof(double); i++){
+    //    std::cout << prices[i] << "\n";
+    //}
+    bool* DynamicSet;
+    DynamicSet = new bool[5];
+    DynamicSet[5] = { false };
+    for (int i=0;i<5;i++) {
+        cout << DynamicSet[i] << endl;
     }
-
+    cout << "--------------------------" << endl;
+    DynamicSet[0] = true;
+    for (int i=0;i<5;i++) {
+        cout << DynamicSet[i] << endl;
+    }
     return 0;
 }
