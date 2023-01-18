0fe4ce433dfefc94dc8d9736fe0fd922e7d35b07
Zayan <particlegriffin@gmail.com>
2023-01-18 10:26:28 +0800

    1004 assign2 menu3 complete

--- UM/1004 assignments/assignment 2/assignment 2.cpp
+++ UM/1004 assignments/assignment 2/assignment 2.cpp
@@ -5,6 +5,8 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <ctime>
+#include <cstdlib>
 
 using namespace std;
 
@@ -13,17 +15,22 @@ string filePath2 = "C:\\Users\\kople\\Documents\\Personal Git repos\\learning_c+
 string studentGroups[100][7] = {};
 
 void menu1(string, string arrayData[][7], int rows);
-void menu2(string arrayData[][7], int rows);
+void menu2(string arrayData[][7], const int rows);
+void menu3(string arrayData[][7], const int rows);
 
 int main()
 {
     menu1(filePath2, studentGroups, 100);
+    cout << "Before randomizing sequent" << endl;
     menu2(studentGroups, 100);
-
+    cout << "---------------------------------------" << endl;
+    menu3(studentGroups, 100);
+    cout << "After randomizing sequent " << endl;
+    menu2(studentGroups, 100);
     return 0;
 }
 
-void menu1(string filePath, string arrayData[][7], int rows) {
+void menu1(string filePath, string arrayData[][7], const int rows) {
     ifstream StudentListcsv;
     StudentListcsv.open(filePath);
 
@@ -54,11 +61,35 @@ void menu1(string filePath, string arrayData[][7], int rows) {
     }
 }
 
-void menu2(string arrayData[][7], int rows) {
+void menu2(string arrayData[][7], const int rows) {
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < 7; j++) {
             cout << arrayData[i][j] << " ";
         }
         cout << endl;
+    }
+}
+
+void menu3(string arrayData[][7], const int rows) {
+    srand(time(0));
+    for (int i = 0; i < rows/2; i++) {
+        int randomIndex = rand() % rows;
+        string tempRecord[7]; //make a temporary array to store record that will be replaced
+
+        if (arrayData[randomIndex][0] != "") { //check if the record at random
+            //store the record that will be replaced later
+            for (int c = 0; c < 7; c++) {
+                tempRecord[c] = arrayData[randomIndex][c];
+            }
+        }
+        //insert current row's record into the random row/index
+        for (int c = 0; c < 7; c++) {
+            arrayData[randomIndex][c] = arrayData[i][c];
+        }
+
+        //insert the record at random index into the current index to complete the swapping
+        for (int c = 0; c < 7; c++) {
+            arrayData[i][c] = tempRecord[c];
+        }
     }
 }
